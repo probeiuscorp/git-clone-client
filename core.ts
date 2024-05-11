83fe6eca49dea1753e467ea70008071d582d4062
Caleb Stimpson <caleb.stimpson@gmail.com>
2024-05-10 22:17:10 -0500

    Add index files

---
+++ core.ts
@@ -0,0 +1,3 @@
+export * from './index';
+export { readObject, readPack, readPacketLines, readTree, readCommitLinks, objectTypeNameById, type GitObject, type GitPack, type GitTreeEntry, type GitPacketLine, type GitCommitLinks } from './git-objects';
+export { findCommitOfRefInAdvertisement } from './git-find-ref';
---
+++ index.ts
@@ -0,0 +1,3 @@
+export { shallowCloneRef, makeFetchLikeRequest, httpFetchUsing, type MakeRequest, type GitRequest, type GitUploadPackRequest, type GitInfoRequest } from './git-clone-client';
+export { findCommitOfRef, maybeFindCommitOfRef } from './git-find-ref';
+export { shallowCloneCommit, isGitModeDirectory, type ShallowCloneCommitOptions } from './git-upload-pack';
