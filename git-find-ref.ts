4369e3e4e5d8031450c42caaa9c7db51930b0a55
Caleb Stimpson <caleb.stimpson@gmail.com>
2024-05-10 21:10:57 -0500

    Add findCommitOfRef

---
+++ git-find-ref.ts
@@ -0,0 +1,37 @@
+import { readPacketLines } from './git-objects';
+import { GitInfoRequest, MakeRequest } from './git-upload-pack';
+
+export function findCommitOfRefInAdvertisement(buffer: Buffer, ref: Buffer) {
+    let hasFlush = false;
+    let isFirst = true;
+    const length = ref.byteLength;
+    for(const line of readPacketLines(buffer)) {
+        if(line.type === 'flush') {
+            hasFlush = true;
+        } else if(hasFlush) {
+            if(isFirst) {
+                isFirst = false;
+            } else {
+                const { payload } = line;
+                const sorting = ref.compare(payload, 41, 41 + length);
+                if(sorting === 0) {
+                    return payload.toString('ascii', 0, 40);
+                }
+            }
+        }
+    }
+    return undefined;
+}
+
+export async function maybeFindCommitOfRef(ref: string, makeRequest: MakeRequest<GitInfoRequest>) {
+    const advertisement = await makeRequest({ type: 'req-info' });
+    return findCommitOfRefInAdvertisement(advertisement, Buffer.from(ref, 'ascii'));
+}
+
+export async function findCommitOfRef(ref: string, makeRequest: MakeRequest<GitInfoRequest>) {
+    const commit = await maybeFindCommitOfRef(ref, makeRequest);
+    if(commit === undefined) {
+        throw new Error(`Could not find ref "${ref}"`);
+    }
+    return commit;
+}
